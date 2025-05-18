6ed32d06155ce2b4d4565ca19ebd04a566158e58
Sadiya-Khanam <khanamsadiya1234@gmail.com>
2025-05-18 20:57:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,25 @@ import react from '@vitejs/plugin-react'*/
   base: '/my-portfolio/', // ← your repo name
 });*/
 
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // 👇 Use your GitHub repo name
 export default defineConfig({
   base: '/my-portfolio/',
   plugins: [react()],
+})*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+export default defineConfig({
+  base: '/my-portfolio/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/App.jsx'),
+    },
+  },
 })
